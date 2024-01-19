6e6c5153086f72f53a73a6f5527cb8954eed0722
Karmpal <kpsheoran33@gmail.com>
2024-01-19 16:53:45 +0530

    Footer added

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,102 @@
+import { Footer, FooterDivider } from "flowbite-react";
+import React from "react";
+import { Link } from "react-router-dom";
+import { BsGithub, BsInstagram, BsLinkedin, BsYoutube } from "react-icons/bs";
+
+export default function FooterCom() {
+  return (
+    <Footer container className="border border-t-8 border-teal-500">
+      <div className="w-full max-w-7xl mx-auto">
+        <div className="grid w-full justify-between sm:flex md:grid-cols-1">
+          <div className="mt-5">
+            <Link
+              to="/"
+              className="self-center whitespace-nowrap text-lg sm:text-xl font-semibold dark:text-white"
+            >
+              <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
+                Karmpal's
+              </span>
+              Blog
+            </Link>
+          </div>
+          <div className="grid grid-cols-2 gap-8 mt-4 sm:grid-cols-3 sm:gap-6">
+            <div>
+              <Footer.Title title="About" />
+              <Footer.LinkGroup col>
+                <Footer.Link
+                  href="https://www.100jsprojects.com"
+                  target="_blank"
+                  rel="noopner noreferrer"
+                >
+                  100 JS Projects
+                </Footer.Link>
+                <Footer.Link
+                  href="https://dholiya-estate.onrender.com"
+                  target="_blank"
+                  rel="noopner noreferrer"
+                >
+                  Dholiya Estate
+                </Footer.Link>
+              </Footer.LinkGroup>
+            </div>
+            <div>
+              <Footer.Title title="Follow us" />
+              <Footer.LinkGroup col>
+                <Footer.Link
+                  href="https://github.com/karmpals"
+                  target="_blank"
+                  rel="noopner noreferrer"
+                >
+                  Github
+                </Footer.Link>
+                <Footer.Link
+                  href="https://www.linkedin.com/in/karmpalsheoran98"
+                  target="_blank"
+                >
+                  LinkedIn
+                </Footer.Link>
+              </Footer.LinkGroup>
+            </div>
+            <div>
+              <Footer.Title title="Legal" />
+              <Footer.LinkGroup col>
+                <Footer.Link href="#">Privacy Policy</Footer.Link>
+                <Footer.Link href="#">Terms &amp; Conditions</Footer.Link>
+              </Footer.LinkGroup>
+            </div>
+          </div>
+        </div>
+        <FooterDivider />
+        <div className='w-full sm:flex sm:items-center sm:justify-between'>
+          <Footer.Copyright
+            href="#"
+            by="Karmpal's blog"
+            year={new Date().getFullYear()}
+          />
+          <div className="flex gap-6 sm:mt-0 mt-4 sm:justify-center">
+            <Footer.Icon
+              href="https://www.instagram.com/karmpal_sheoran/"
+              target="_blank"
+              icon={BsInstagram}
+            />
+            <Footer.Icon
+              href="https://www.youtube.com/channel/UChZ1AEdU8DRb7JynjnZvKew"
+              target="_blank"
+              icon={BsYoutube}
+            />
+            <Footer.Icon
+              href="https://www.linkedin.com/in/karmpalsheoran98"
+              target="_blank"
+              icon={BsLinkedin}
+            />
+            <Footer.Icon
+              href="https://github.com/karmpals"
+              target="_blank"
+              icon={BsGithub}
+            />
+          </div>
+        </div>
+      </div>
+    </Footer>
+  );
+}
