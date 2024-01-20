e11deea6d3ca2d3ea0d49ed85f58ea7c281684b7
Karmpal <kpsheoran33@gmail.com>
2024-01-20 12:27:11 +0530

    private route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,17 +8,20 @@ import Signup from "./pages/Signup";
 import Projects from "./pages/Projects";
 import Header from "./components/header";
 import FooterCom from "./components/Footer";
+import PrivateRoute from "./components/PrivateRoute";
 
 export default function App() {
   return (
     <BrowserRouter>
-    <Header />
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-        <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/sign-in" element={<Signin />} />
         <Route path="/sign-up" element={<Signup />} />
+        <Route element={<PrivateRoute />}>
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Route>
         <Route path="/projects" element={<Projects />} />
       </Routes>
       <FooterCom />
