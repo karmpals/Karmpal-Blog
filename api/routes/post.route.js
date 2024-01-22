35a17b2746ca0b9e80698a59263d6d1d8c287a92
Karmpal <kpsheoran33@gmail.com>
2024-01-22 16:43:17 +0530

    update post

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -3,6 +3,7 @@ import {
   create,
   deletepost,
   getposts,
+  updatepost,
 } from "../controllers/post.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
@@ -11,5 +12,6 @@ const router = express.Router();
 router.post("/create", verifyToken, create);
 router.get("/getposts", getposts);
 router.delete("/deletepost/:postId/:userId", verifyToken, deletepost);
+router.put("/updatepost/:postId/:userId", verifyToken, updatepost);
 
 export default router;
